cb150279e9af34a1cd1899dee99a38f81dfd9d19
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-28 18:41:40 -0400

    Added and Configure Chakra UI

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import {
+  Html,
+  Head,
+  NextScript,
+  Main,
+} from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
